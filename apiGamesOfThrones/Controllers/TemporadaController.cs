8f1b204133c074da788c2a2fbce1a9b5447c0e97
Ariel Alexis Lorda Barrabasqui <Ariel.Lorda@inworx.com>
2021-10-28 10:35:02 -0300

    OK

---
+++ apiGamesOfThrones/Controllers/TemporadaController.cs
@@ -0,0 +1,115 @@
+﻿using apiGamesOfThrones.Context;
+using apiGamesOfThrones.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace apiGamesOfThrones.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TemporadaController : ControllerBase
+    {
+
+        private readonly AppDbContext context; 
+        public TemporadaController(AppDbContext context)
+        {
+            this.context = context;
+
+        }
+        // GET: api/<TemporadaController>
+        [HttpGet]
+        public ActionResult Get()
+        {
+            try
+            {
+                return Ok(context.Temporada.ToList());
+            }catch(Exception ex)
+            {
+                return BadRequest(ex.Message);            }
+        }
+
+        // GET api/<TemporadaController>/5
+        [HttpGet("{id}",Name ="GetTemporada")]
+        public ActionResult Get(int id)
+        {
+            try
+            {
+                var temporada = context.Temporada.FirstOrDefault(t => t.id == id);
+                return Ok(temporada);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        // POST api/<TemporadaController>
+        [HttpPost]
+        public ActionResult Post([FromBody] Temporada temporada)
+        {
+            try
+            {
+                context.Temporada.Add(temporada);
+                context.SaveChanges();
+                return CreatedAtRoute("GetTemporada",new { id = temporada.id }, temporada);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // PUT api/<TemporadaController>/5
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] Temporada temporada)
+        {
+            try
+            {
+                if(temporada.id == id)
+                {
+                    context.Entry(temporada).State = EntityState.Modified;
+                    context.SaveChanges();
+                    return CreatedAtRoute("GetTemporada", new { id = temporada.id }, temporada);
+                    ;
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // DELETE api/<TemporadaController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var temporada = context.Temporada.FirstOrDefault(t => t.id == id);
+                if(temporada != null)
+                {
+                    context.Temporada.Remove(temporada);
+                    context.SaveChanges();
+                    return Ok(id);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ apiGamesOfThrones/Models/Temporada.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace apiGamesOfThrones.Models
+{
+    public class Temporada
+    {
+       
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string anio { get; set; }
+        public string cantEpisodios { get; set; }
+        public string libro { get; set; }
+        public Double rating { get; set; }
+        public string foto { get; set; }
+    }
+}
